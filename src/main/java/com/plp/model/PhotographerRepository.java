475e1bbdf6802a6d6b85f4f612410b293a3c7ff6
DulakshanaMalith <146856013+DulakshanaMalith@users.noreply.github.com>
2025-04-26 17:39:51 +0530

    reverting-changes

--- src/main/java/com/plp/model/PhotographerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.plp.repository;
-
-import com.plp.model.Photographer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PhotographerRepository extends MongoRepository<Photographer, String> {
-} 
